2942ebac089f469da7902352c037bece1df726ba
seongjun-yang1537 <seongjun.yang1537@5minlab.com>
2025-11-27 21:17:45 +0900

    save

---
+++ Assets/Scripts/Underforge/Editor/SDF/SDFGenerateConfigInspector.cs
@@ -0,0 +1,12 @@
+using Corelib.SUI;
+
+namespace Underforge
+{
+    public static class SDFGenerateConfigInspector
+    {
+        public static SUIElement Render(SDFGenerateConfig config)
+        {
+            return SUIElement.Empty();
+        }
+    }
+}
