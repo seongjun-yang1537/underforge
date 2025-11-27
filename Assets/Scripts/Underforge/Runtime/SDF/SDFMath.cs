690aa032bb4a19e91bdab719526759811175e778
seongjun-yang1537 <seongjun.yang1537@5minlab.com>
2025-11-27 21:27:06 +0900

    save

---
+++ Assets/Scripts/Underforge/Runtime/SDF/SDFMath.cs
@@ -0,0 +1,23 @@
+using Unity.Mathematics;
+using static Unity.Mathematics.math;
+
+namespace Underforge
+{
+    public static class SDFMath
+    {
+        public static float3 Interpolate(float3 p1, float3 p2, float v1, float v2)
+        {
+            float t = -v1 / (v2 - v1);
+            return lerp(p1, p2, t);
+        }
+
+        public static float3 CalculatenormalFromGrid(ref SDFVolume volume, int3 idx)
+        {
+            float dx = volume[idx + int3(1, 0, 0)] - volume[idx - int3(1, 0, 0)];
+            float dy = volume[idx + int3(0, 1, 0)] - volume[idx - int3(0, 1, 0)];
+            float dz = volume[idx + int3(0, 0, 1)] - volume[idx - int3(0, 0, 1)];
+
+            return normalize(float3(dx, dy, dz));
+        }
+    }
+}
